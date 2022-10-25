c99c9acbb68f1f1a12c989424008291e9e75543b
Kanazawanaoaki <naoaki65k@gmail.com>
2022-10-25 18:38:32 +0900

    Fix dirctory

--- jsk_2022_10_egg_dishes/scripts/googletrans_test.py
+++ jsk_2022_10_egg_dishes/scripts/googletrans_test.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import argparse
 from googletrans import Translator
 
 translator = Translator()
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-t','--text', default='沸騰している水')
+
+args = parser.parse_args()
+query_text = args.text
+
 # ans = translator.translate('안녕하세요.', dest='ja')
 # ans = translator.translate('水が沸騰している', dest='en')
-
 # ans = translator.translate('沸騰している水', dest='en')
+
+ans = translator.translate(query_text, dest='en')
 print(ans)
 print(ans.text)
--- jsk_2022_10_egg_dishes/scripts/mecab_test.py
+++ jsk_2022_10_egg_dishes/scripts/mecab_test.py
@@ -1,15 +1,19 @@
 #! /usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import sys
 import MeCab
+import argparse
 
 
 if __name__ == "__main__":
     t = MeCab.Tagger()
-    file_name = "test.txt"
-    if len(sys.argv)>=2:
-        file_name = sys.argv[1]
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f','--file', default="test.txt")
+
+    args = parser.parse_args()
+    file_name = args.file
+    print("text file loaded from {}".format(file_name))
 
     f = open(file_name, 'r')
     for i, line in enumerate(f):
