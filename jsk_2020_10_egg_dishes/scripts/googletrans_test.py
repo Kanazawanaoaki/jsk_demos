022c120f07d8930f57356927143d9c8dcea7f782
Kanazawa Naoaki <naoaki65k@gmail.com>
2022-10-25 16:34:44 +0900

    Make repository

---
+++ jsk_2020_10_egg_dishes/scripts/googletrans_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from googletrans import Translator
+
+translator = Translator()
+# ans = translator.translate('안녕하세요.', dest='ja')
+# ans = translator.translate('水が沸騰している', dest='en')
+
+# ans = translator.translate('沸騰している水', dest='en')
+print(ans)
+print(ans.text)
