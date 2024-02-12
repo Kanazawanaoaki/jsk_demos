6c932e09d0764af6e0f9213187508c0e082d9e4c
Kanazawanaoaki <naoaki65k@gmail.com>
2024-02-12 16:44:59 +0900

    Add exec test

--- jsk_teaching_object/scripts/calc_iou_yolo_test.py
+++ jsk_teaching_object/scripts/calc_iou_yolo_test.py
@@ -25,39 +25,26 @@ def draw_result(image, results):
 
     # import ipdb
     # ipdb.set_trace()
+    for boxes in results[0].boxes:
+        box = boxes.xyxy[0].numpy()
+        x1, y1, x2, y2, confidence, class_index = box
+        label = f'Class {int(class_index)} - Confidence: {confidence:.2f}'
+        annotation['shapes'].append({
+            'label': label,
+            'points': [(x1, y1), (x2, y2)],
+            'shape_type': 'rectangle'
+        })
 
-    # result_array = np.array(result.plot())
-    # result_image = Image.fromarray(result_array)
+    for shape in shapes:
+        label = shape.get('label', '')
+        points = shape.get('points', [])
+        shape_type = shape.get('shape_type', 'rectangle')
 
-    result_array_bgr = np.array(result.plot())
-    result_array_rgb = result_array_bgr[..., ::-1]
-    result_image = Image.fromarray(result_array_rgb)
-
-    image = result_image
-    return image
-
-    # # import ipdb
-    # # ipdb.set_trace()
-    # for boxes in results[0].boxes:
-    #     box = boxes.xyxy[0].numpy()
-    #     x1, y1, x2, y2, confidence, class_index = box
-    #     label = f'Class {int(class_index)} - Confidence: {confidence:.2f}'
-    #     annotation['shapes'].append({
-    #         'label': label,
-    #         'points': [(x1, y1), (x2, y2)],
-    #     'shape_type': 'rectangle'
-    #     })
-
-    # for shape in shapes:
-    #     label = shape.get('label', '')
-    #     points = shape.get('points', [])
-    #     shape_type = shape.get('shape_type', 'rectangle')
-
-    #     if shape_type == 'rectangle' and len(points) == 2:
-    #         x1, y1 = points[0]
-    #         x2, y2 = points[1]
-    #         draw.rectangle([x1, y1, x2, y2], outline='red', width=2)
-    #         draw.text((x1, y1), label, fill='red')
+        if shape_type == 'rectangle' and len(points) == 2:
+            x1, y1 = points[0]
+            x2, y2 = points[1]
+            draw.rectangle([x1, y1, x2, y2], outline='red', width=2)
+            draw.text((x1, y1), label, fill='red')
 
 def process_image(input_folder, output_folder, yolo_model_path):
     yolo_model = YOLO(yolo_model_path)
@@ -73,7 +60,7 @@ def process_image(input_folder, output_folder, yolo_model_path):
                 results = yolo_model(image_path)
 
                 # Draw annotation on the image
-                image = draw_result(image, results)
+                draw_result(image, results)
 
                 # Save the annotated image to the output folder
                 output_path = os.path.join(output_folder, filename.replace(".png", "_result.png"))
