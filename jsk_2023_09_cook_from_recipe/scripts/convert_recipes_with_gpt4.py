fe708d64597d505d63cfd391f35e3ce9c4b5bb97
Kanazawanaoaki <naoaki65k@gmail.com>
2024-03-09 19:34:53 +0900

    Update convert_recipes_with_gpt4.py

--- jsk_2023_09_cook_from_recipe/scripts/convert_recipes_with_gpt4.py
+++ jsk_2023_09_cook_from_recipe/scripts/convert_recipes_with_gpt4.py
@@ -1,5 +1,6 @@
 import openai
 import argparse
+import os
 
 def generate_text(prompt, conversation_history, temperature):
     # プロンプトを会話履歴に追加
@@ -32,16 +33,21 @@ def read_text_from_file(file_path):
         print(f"An error occurred: {str(e)}")
         return None
 
+def extract_file_name(file_path):
+    file_name = os.path.splitext(os.path.basename(file_path))[0]
+    return file_name
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-k','--key', default="", help="受け取ったAPI key")
-    # parser.add_argument('-p','--prompt', default="../recipes/prompts/egg-recipes-jp.txt", help="プロンプトテキストへのパス")
+    parser.add_argument('-t','--temperature', default=0.0, type=float)
+    # parser.add_argument('-p','--prompt', default="../recipes/prompts/egg-recipes-prompt-jp.txt", help="プロンプトテキストへのパス")
     # parser.add_argument('-r','--recipe', default="../recipes/new_recipes/butter-sunny-jp.txt", help="未知のレシピのテキストへのパス")
     # parser.add_argument('-l','--language', default="jp", help="使用する自然言語の言語")
-    parser.add_argument('-p','--prompt', default="../recipes/prompts/egg-recipes.txt", help="プロンプトテキストへのパス")
+    parser.add_argument('-p','--prompt', default="../recipes/prompts/egg-recipes-prompt.txt", help="プロンプトテキストへのパス")
     parser.add_argument('-r','--recipe', default="../recipes/new_recipes/butter-sunny.txt", help="未知のレシピのテキストへのパス")
     parser.add_argument('-l','--language', choices=['en', 'jp'], default="en", help="使用する自然言語の言語 (en, jp)")
-    parser.add_argument('-t','--temperature', default=0.0, type=float)
+    parser.add_argument('-o','--output_dir', default="../recipes/output_seqs/", help="出力のフォルダへのパス")
 
     args = parser.parse_args()
     openai.api_key = args.key
@@ -49,6 +55,7 @@ if __name__ == "__main__":
     prompt_text_path = args.prompt
     recipe_text_path = args.recipe
     use_lang = args.language
+    output_dir_path = args.output_dir
 
     prompt_text = read_text_from_file(prompt_text_path)
     recipe_text = read_text_from_file(recipe_text_path)
@@ -67,6 +74,15 @@ if __name__ == "__main__":
     generated_text = generate_text(input_prompt, conversation_history, temperature)
     print("\n応答:")
     print(generated_text)
+
+    ## save to file
+    prompt_name = extract_file_name(prompt_text_path)
+    recipe_name = extract_file_name(recipe_text_path)
+
+    output_file_path = os.path.join(output_dir_path, f"{prompt_name}_{recipe_name}_converted.txt")
+    with open(output_file_path, 'w', encoding='utf-8') as file:
+        file.write(generated_text)
+    print("Out file saved in {}".format(output_file_path))
 
     # while True:
     #     # ユーザーに質問を入力させる
