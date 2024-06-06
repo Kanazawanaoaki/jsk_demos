f79cb18097af016bb2f4ae13171d306cebb0efaa
Kanazawanaoaki <naoaki65k@gmail.com>
2024-06-06 15:44:35 +0900

    tmp

--- jsk_2023_09_cook_from_recipe/node_scripts/open3d/merge_with_python_cloud.py
+++ jsk_2023_09_cook_from_recipe/node_scripts/open3d/merge_with_python_cloud.py
@@ -5,6 +5,7 @@ import sensor_msgs.point_cloud2 as pc2
 import numpy as np
 import ctypes
 import struct
+from collections import deque
 from sensor_msgs.msg import PointCloud2, PointField
 from std_msgs.msg import Header
 import sys
@@ -22,39 +23,37 @@ class MergePointClouds:
     def __init__(self):
         self.pub = rospy.Publisher("/merged_point_cloud", PointCloud2, queue_size=2)
         self.num_ptcloud = 5
+
         self.ptcloud_count = 0
+        self.latest_ptclouds = deque(maxlen=self.num_ptcloud)
+        self.current_point = None
 
-    def ptcloud_callback_white(self,ptcloud):
-        self.ptcloud_white = ptcloud
+    def ptcloud_callback(self, ptcloud):
+        self.current_point = ptcloud
         self.ptcloud_count = self.ptcloud_count + 1
-        print "new pointcloud available ptcloud_count:%d " % (ptcloud_count)
-        self.points_white = self.read_pointcloud(ptcloud)
-        if self.n_brown == self.n_white:
-            print "publish in white"
+        print("new pointcloud available ptcloud_count:%d " % (self.ptcloud_count))
+        self.latest_ptclouds.append(self.read_pointcloud(ptcloud))
+        print(len(self.latest_ptclouds))
+        if len(self.latest_ptclouds) >= self.num_ptcloud:
+            print("publish in ptcloud")
             ptcloud_merged = self.generate_pointcloud()
             self.pub.publish(ptcloud_merged)
 
-    def callback_brown(self,ptcloud):
-        self.ptcloud_brown = ptcloud
-        self.n_brown = self.n_brown + 1
-        self.points_brown = self.read_pointcloud(ptcloud)
-        print "new brown pointcloud available n_brown:%d  n_white:%d" % (self.n_brown,self.n_white)
-        if self.n_brown == self.n_white:
-            print "publish in brown"
-            ptcloud_merged = self.generate_pointcloud()
-            self.pub.publish(ptcloud_merged)
-
-
-    def read_pointcloud(self,ptcloud):
+    def read_pointcloud(self, ptcloud):
         fields = ptcloud.fields
         fields[3].datatype = 6
         points = pc2.read_points_list(ptcloud, skip_nans=True)
         return points
 
     def generate_pointcloud(self):
-        header =  self.ptcloud_brown.header
-        fields = self.ptcloud_brown.fields
-        points = self.points_brown + self.points_white
+        header =  self.current_point.header
+        fields = self.current_point.fields
+        points = None
+        for ptcloud in self.latest_ptclouds:
+            if points is None:
+                points = ptcloud
+            else:
+                points = points + ptcloud
         ptcloud_merged = pc2.create_cloud(header, fields, points)
         ptcloud_merged.fields[3].datatype = 7
         return ptcloud_merged
