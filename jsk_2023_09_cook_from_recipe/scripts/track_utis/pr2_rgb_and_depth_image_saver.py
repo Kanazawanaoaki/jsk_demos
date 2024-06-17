e00741fc87e60be0489614a26740f94dd5ac8d1c
Kanazawanaoaki <naoaki65k@gmail.com>
2024-06-17 13:57:35 +0900

    Add approximate sync

--- jsk_2023_09_cook_from_recipe/scripts/track_utis/pr2_rgb_and_depth_image_saver.py
+++ jsk_2023_09_cook_from_recipe/scripts/track_utis/pr2_rgb_and_depth_image_saver.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
+import argparse
 import message_filters
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
@@ -11,9 +12,11 @@ from std_srvs.srv import Empty, EmptyResponse
 
 # Global variable to control the start/stop state
 is_running = False
+data_path = None
 
 def callback(rgb_msg, depth_msg):
     global is_running
+    global data_path
     if not is_running:
         return
 
@@ -27,7 +30,7 @@ def callback(rgb_msg, depth_msg):
     current_time = str(int(time.time() * 1000))  # Convert to milliseconds and convert to string
 
     # Get the specified folder from ROS parameters
-    specified_folder = rospy.get_param('~specified_folder', '../data/object_datas/images')
+    specified_folder = data_path
 
     # Create directories if they don't exist
     rgb_folder = os.path.join(specified_folder, 'rgb')
@@ -60,7 +63,14 @@ def stop_service(req):
     return EmptyResponse()
 
 def main():
-    rospy.init_node('image_synchronizer', anonymous=True)
+    parser = argparse.ArgumentParser(description='Save synchronized RGB and depth images from ROS topic.')
+
+    parser.add_argument('--data_save_path', '-d', default='../../datas/object_datas/images',type=str, help='Path to save image dir.')
+    parser.add_argument('--approximate', '-a', action='store_true', help='use approixmate.')
+
+    args = parser.parse_args()
+
+    rospy.init_node('pr2_image_synchronizer', anonymous=True)
 
     # Define the topics to subscribe to
     # rgb_topic = '/camera_remote/rgb/image_raw'
@@ -72,13 +82,19 @@ def main():
     rgb_sub = message_filters.Subscriber(rgb_topic, Image)
     depth_sub = message_filters.Subscriber(depth_topic, Image)
 
+    global data_path
+    data_path = args.data_save_path
+
     # Synchronize the topics
-    ts = message_filters.TimeSynchronizer([rgb_sub, depth_sub], 10)
+    if args.approximate:
+        ts = message_filters.ApproximateTimeSynchronizer([rgb_sub, depth_sub], 10, 0.1)
+    else:
+        ts = message_filters.TimeSynchronizer([rgb_sub, depth_sub], 10)
     ts.registerCallback(callback)
 
     # Define the start and stop services
-    start_srv = rospy.Service('start_sync', Empty, start_service)
-    stop_srv = rospy.Service('stop_sync', Empty, stop_service)
+    start_srv = rospy.Service('pr2_saver_start_sync', Empty, start_service)
+    stop_srv = rospy.Service('pr2_saver_stop_sync', Empty, stop_service)
 
     rospy.loginfo("Ready to start and stop image synchronization.")
     rospy.loginfo(f'Save topic: rgb is {rgb_topic}, depth is {depth_topic}.')
