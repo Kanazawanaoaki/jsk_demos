b800d43b68a2a489ca9429a397d120c46bcc656c
Kanazawanaoaki <naoaki65k@gmail.com>
2024-11-18 18:50:50 +0900

    Update femto mega tgf

--- jsk_2023_09_cook_from_recipe/node_scripts/femto_mega/femto_mega_tf_publisher.py
+++ jsk_2023_09_cook_from_recipe/node_scripts/femto_mega/femto_mega_tf_publisher.py
@@ -15,7 +15,7 @@ def broadcast_tf():
     while not rospy.is_shutdown():
         # head_mount_linkからcamera_baseへの座標変換を指定
         # translation = (0.175, 0, 0.025)  # 例えば、x、y、zは適切な値に置き換える必要があります
-        translation = (0.170, -0.04, -0.010)  # 例えば、x、y、zは適切な値に置き換える必要があります
+        translation = (0.160, -0.04, -0.010)  # 例えば、x、y、zは適切な値に置き換える必要があります
         rotation = (0, 0, 0, 1)  # 回転の場合も同様です
 
         broadcaster.sendTransform(
