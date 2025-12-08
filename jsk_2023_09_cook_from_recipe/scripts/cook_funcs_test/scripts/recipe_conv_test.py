254ce48d9db3a19804fc8754391d33996b42cbf7
Kanazawanaoaki <naoaki65k@gmail.com>
2025-12-08 14:07:04 +0900

    Update recipe_conv_test.py

--- jsk_2023_09_cook_from_recipe/scripts/cook_funcs_test/scripts/recipe_conv_test.py
+++ jsk_2023_09_cook_from_recipe/scripts/cook_funcs_test/scripts/recipe_conv_test.py
@@ -14,12 +14,17 @@ def read_text_from_file(file_path):
         print(f"An error occurred: {str(e)}")
         return None
 
+def extract_file_name(file_path):
+    file_name = os.path.splitext(os.path.basename(file_path))[0]
+    return file_name
+
 def main():
     # ===== argparse で引数を定義 =====
     parser = argparse.ArgumentParser(
         description="Azure OpenAI でレシピを関数に変換するスクリプト"
     )
     parser.add_argument(
+        "-k",
         "--api-key",
         required=True,
         help="Azure OpenAI の API キーを指定してください",
@@ -48,7 +53,7 @@ def main():
     parser.add_argument(
         '-r',
         '--recipe',
-        default="../texts/recipes/sauteed-broccoli.txt",
+        default="../texts/en_recipes/sauteed-broccoli.txt",
         help="未知のレシピのテキストへのパス"
     )
     parser.add_argument(
@@ -60,6 +65,17 @@ def main():
 
 
     args = parser.parse_args()
+
+    prompt_text_path = args.prompt
+    recipe_text_path = args.recipe
+    output_dir_path = args.output_dir
+
+
+    ## 入力するテキストプロンプトを作成
+    prompt_text = read_text_from_file(prompt_text_path)
+    recipe_text = read_text_from_file(recipe_text_path)
+    input_text = prompt_text + "\n[Recipe]\n" + recipe_text + "[Function Sequence]"
+    # print(input_text)
 
     # ===== AzureOpenAI クライアントを作成 =====
     client = AzureOpenAI(
@@ -77,14 +93,25 @@ def main():
             },
             {
                 "role": "user",
-                "content": "I am going to Paris, what should I see?",
+                # "content": "I am going to Paris, what should I see?",
+                "content": input_text,
             }
         ],
         max_completion_tokens=16384,
         model=args.deployment,
     )
 
-    print(response.choices[0].message.content)
+    output_text = response.choices[0].message.content
+    print(output_text)
+
+    ## 出力結果を保存
+    prompt_name = extract_file_name(prompt_text_path)
+    recipe_name = extract_file_name(recipe_text_path)
+
+    output_file_path = os.path.join(output_dir_path, f"{args.deployment}_{args.api_version}-{prompt_name}_{recipe_name}_converted.txt")
+    with open(output_file_path, 'w', encoding='utf-8') as file:
+        file.write(output_text)
+    print("\nOutput is saved in {}".format(output_file_path))
 
 
 if __name__ == "__main__":
